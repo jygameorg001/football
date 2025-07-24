226bc1684ce99f7befea9aacb2b478b9bb07d8d4
joge <>
2025-07-24 23:50:20 +0800

    测试运动轨迹

--- temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,7 +0,0 @@
-
-require('./assets/Script/Game');
-require('./assets/Script/Home');
-require('./assets/Script/Shoot');
-require('./assets/Script/common/AudioMgr');
-require('./assets/Script/common/EventManager');
-require('./assets/Script/common/HttpHelper');
