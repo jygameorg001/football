0b7980c69cc3aecb9448f9a8b118f541fbf5a511
leaf <leaf@gmail.com>
2025-07-30 19:26:30 +0800

    10次逻辑显示面板绑定

--- assets/Script/RewardListview.ts
+++ assets/Script/RewardListview.ts
@@ -1,12 +1,15 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class rewardListview extends cc.Component {
 
     @property(cc.Prefab)
-    rewardItem:cc.Prefab = null;
+    rewardItem: cc.Prefab = null;
 
-    start () {
+    @property(cc.Node)
+    contentView: cc.Node = null;
+
+    start() {
 
     }
 
---
+++ assets/Script/TenItem.ts
@@ -0,0 +1,12 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class TenItem extends cc.Component {
+
+
+   setview(data){
+       
+   }
+
+
+}
