4d99f77ef9d7728bd98023e025cdbd696fa11bf1
hexuejie <hexuejie@jinyikj.com.cn>
2025-08-12 16:31:54 +0800

    feat: 1

--- assets/Script/svga-cocos/cocos/svga-pool.ts
+++ assets/Script/svga-cocos/cocos/svga-pool.ts
@@ -1,164 +1,168 @@
 import { SVGADynamicImage, SVGADynamicImageType } from "./svga-player";
 
+type SpriteFrameMap = { [key: string]: cc.SpriteFrame };
+type AudioClipMap = { [key: string]: cc.AudioClip };
+type SvgaMarkMap = { [svgaMark: string]: { [key: string]: boolean } };
 
 export default class SVGAPool {
-    private _svgaMark: any = {};
-    static _spriteFrames: any = {};
-    private _audioClips: any = {};
-    static loadKey: any={};
-    // public static svgaFrameCaches ={};
+  private _svgaMark: SvgaMarkMap = {};
+  static _spriteFrames: SpriteFrameMap = {};
+  private _audioClips: AudioClipMap = {};
+  static loadKey: any = {};
 
-    private _getSpriteFrame(name: string, svgaMark: string): cc.SpriteFrame {
-        if (SVGAPool._spriteFrames[name]) {
-            this.pushSvgaMarkData(name, svgaMark);
-            return SVGAPool._spriteFrames[name];
-        }
-        return null;
+  private _getSpriteFrame(
+    name: string,
+    svgaMark: string
+  ): cc.SpriteFrame | null {
+    const sf = SVGAPool._spriteFrames[name];
+    if (sf) {
+      this.pushSvgaMarkData(name, svgaMark);
+      return sf;
     }
+    return null;
+  }
 
-    private _pushSpriteFrame(name: string, svgaMark: string, sf: cc.SpriteFrame) {
-        SVGAPool._spriteFrames[name] = sf;
-        this.pushSvgaMarkData(name, svgaMark);
-        sf.addRef();
+  private _pushSpriteFrame(name: string, svgaMark: string, sf: cc.SpriteFrame) {
+    SVGAPool._spriteFrames[name] = sf;
+    this.pushSvgaMarkData(name, svgaMark);
+    sf.addRef();
+  }
+
+  private _getAudioClip(name: string, svgaMark: string): cc.AudioClip | null {
+    const ac = this._audioClips[name];
+    if (ac) {
+      this.pushSvgaMarkData(name, svgaMark);
+      return ac;
     }
+    return null;
+  }
 
-    private _getAudioClip(name: string, svgaMark: string): cc.AudioClip {
-        if (this._audioClips[name]) {
-            this.pushSvgaMarkData(name, svgaMark);
-            return this._audioClips[name];
-        }
-        return null;
+  private _pushAudioClip(name: string, svgaMark: string, ac: cc.AudioClip) {
+    this._audioClips[name] = ac;
+    this.pushSvgaMarkData(name, svgaMark);
+    ac.addRef();
+  }
+
+  // 记录资源和 svgaMark 的引用关系
+  pushSvgaMarkData(name: string, svgaMark: string) {
+    if (!svgaMark) return;
+    if (!this._svgaMark[svgaMark]) {
+      this._svgaMark[svgaMark] = {};
     }
+    this._svgaMark[svgaMark][name] = true;
+  }
 
-    private _pushAudioClip(name: string, svgaMark: string, ac: cc.AudioClip) {
-        this._audioClips[name] = ac;
-        this.pushSvgaMarkData(name, svgaMark);
-        ac.addRef();
+  getSpriteFrame(
+    dynamicImageData: SVGADynamicImage,
+    svgaMark: string,
+    cb: (sf: cc.SpriteFrame | null) => void
+  ) {
+    if (!dynamicImageData || !cb) {
+      cc.error("data or callback is null");
+      cb?.(null);
+      return;
     }
-
-    pushSvgaMarkData(name: string, svgaMark: string) {
-        // if (!this._svgaMark[svgaMark]) {
-        //     this._svgaMark[svgaMark] = {};
-        // }
-        // if (!this._svgaMark[svgaMark][name]) {
-        //     this._svgaMark[svgaMark][name] = true;
-        // }
+    let spu = "";
+    if (dynamicImageData.type === SVGADynamicImageType.RemoteUrl) {
+      spu = dynamicImageData.remoteUrl;
+    } else {
+      spu = "data:image/png;base64," + dynamicImageData.base64;
     }
-
-    getSpriteFrame(dynamicImageData: SVGADynamicImage, svgaMark: string, cb: Function) {
-        if (!dynamicImageData || !cb) {
-            cc.error("data or callback is null");
-            if (cb) {
-                cb(null);
-            }
+    const key = dynamicImageData.imageKey;
+    const sf = this._getSpriteFrame(key, svgaMark);
+    if (sf) {
+      cb(sf);
+    } else {
+      cc.assetManager.loadRemote(
+        spu,
+        { ext: ".png" },
+        (err, texture: cc.Texture2D) => {
+          if (err) {
+            cc.error("image spu", err);
+            cb(null);
             return;
+          }
+          const spriteFrame = new cc.SpriteFrame(texture);
+          this._pushSpriteFrame(key, svgaMark, spriteFrame);
+          cb(spriteFrame);
         }
-        let spu = "";
-        if (dynamicImageData.type == SVGADynamicImageType.RemoteUrl) {
-            spu = dynamicImageData.remoteUrl;
-        }
-        else {
-            spu = "data:image/png;base64," + dynamicImageData.base64;
-        }
-        let key = dynamicImageData.imageKey;
-        let sf = this._getSpriteFrame(key, svgaMark);
-        if (sf) {
-            cb(sf);
-        }
-        else {
-            cc.assetManager.loadRemote(spu, { ext: '.png' }, (err, texture: cc.Texture2D) => {
-                if (err) {
-                    cc.error("image spu", err);
-                    cb(null);
-                    return;
-                }
-                // console.log("====getSprite Frame=====",key)
-                let sf = new cc.SpriteFrame(texture);
-                this._pushSpriteFrame(key, svgaMark, sf);
-                cb(sf);
-                spu = null;
-            });
-        }
+      );
     }
-    getAudioClip(base64Data: string, svgaMark: string, cb: Function) {
-        if (!base64Data || !cb) {
-            cc.error("data or callback is null");
-            if (cb) {
-                cb(null);
-            }
+  }
+
+  getAudioClip(
+    base64Data: string,
+    svgaMark: string,
+    cb: (ac: cc.AudioClip | null) => void
+  ) {
+    if (!base64Data || !cb) {
+      cc.error("data or callback is null");
+      cb?.(null);
+      return;
+    }
+    const spu = "data:audio/x-mpeg;base64," + base64Data;
+    const ac = this._getAudioClip(spu, svgaMark);
+    if (ac) {
+      cb(ac);
+    } else {
+      cc.assetManager.loadRemote(
+        spu,
+        { ext: ".mp3" },
+        (err, audioClip: cc.AudioClip) => {
+          if (err) {
+            cc.error("audio spu", err);
+            cb(null);
             return;
+          }
+          this._pushAudioClip(spu, svgaMark, audioClip);
+          cb(audioClip);
         }
-        console.log("===getAudioClip===1==");
-        const spu = "data:audio/x-mpeg;base64," + base64Data;
-        let ac = this._getAudioClip(spu, svgaMark);
-        if (ac) {
-            cb(ac);
-        }
-        else {
-            cc.assetManager.loadRemote(spu, { ext: '.mp3' }, (err, ac: cc.AudioClip) => {
-                if (err) {
-                    cc.error("audio spu", err);
-                    cb(null);
-                    return;
-                }
-                this._pushAudioClip(spu, svgaMark, ac);
-                cb(ac);
-            });
-        }
+      );
     }
+  }
 
-    // 防止释放时，同一个资源在多个svga中使用
-    private _getClearListBySvgaMark(svgaMark: string) {
-        let clearList: string[] = [];
-        if (!this._svgaMark[svgaMark]) {
-            return clearList;
+  // 获取某 svgaMark 独占的资源 key 列表
+  private _getClearListBySvgaMark(svgaMark: string): string[] {
+    const clearList: string[] = [];
+    const markData = this._svgaMark[svgaMark];
+    if (!markData) return clearList;
+
+    for (const key in markData) {
+      let isShared = false;
+      for (const otherMark in this._svgaMark) {
+        if (otherMark === svgaMark) continue;
+        if (this._svgaMark[otherMark][key]) {
+          isShared = true;
+          break;
         }
-        for (let key in this._svgaMark[svgaMark]) {
-            let isHave = false;
-            for (let key in this._svgaMark) {
-                if (key == svgaMark) {
-                    continue;
-                }
-                for (let kk in this._svgaMark[key]) {
-                    if (kk == key) {
-                        isHave = true;
-                        break;
-                    }
-                }
-                if (isHave) {
-                    break;
-                }
-            }
-            if (!isHave) {
-                clearList.push(key);
-            }
-        }
-        return clearList;
+      }
+      if (!isShared) {
+        clearList.push(key);
+      }
     }
+    return clearList;
+  }
 
-    public clearBySvgaMark(svgaMark: string) {
-        let clearList = this._getClearListBySvgaMark(svgaMark);
-        if (this._svgaMark[svgaMark]) {
-            delete this._svgaMark[svgaMark];
-        }
-        for (let i = 0; i < clearList.length; ++i) {
-            // if (this._spriteFrames[clearList[i]]) {
-            //     this._spriteFrames[clearList[i]].getTexture().decRef();
-            //     this._spriteFrames[clearList[i]].decRef();
-            //     delete this._spriteFrames[clearList[i]];
-            // }
-            if (this._audioClips[clearList[i]]) {
-                this._audioClips[clearList[i]].decRef();
-                delete this._audioClips[clearList[i]];
-            }
-        }
+  public clearBySvgaMark(svgaMark: string) {
+    const clearList = this._getClearListBySvgaMark(svgaMark);
+    delete this._svgaMark[svgaMark];
+    for (const key of clearList) {
+      if (SVGAPool._spriteFrames[key]) {
+        SVGAPool._spriteFrames[key].getTexture()?.decRef();
+        SVGAPool._spriteFrames[key].decRef();
+        delete SVGAPool._spriteFrames[key];
+      }
+      if (this._audioClips[key]) {
+        this._audioClips[key].decRef();
+        delete this._audioClips[key];
+      }
     }
+  }
 
-    public clearAllBySvgaMark() {
-        for (const key in this._svgaMark) {
-            this.clearBySvgaMark(key);
-
-            delete this._svgaMark[key];
-        }
+  public clearAllBySvgaMark() {
+    for (const key in this._svgaMark) {
+      this.clearBySvgaMark(key);
     }
+    this._svgaMark = {};
+  }
 }
