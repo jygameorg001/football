1112fafca9aee33b2928e62b22ce58772a907bb4
hexuejie <hexuejie@jinyikj.com.cn>
2025-08-14 17:26:11 +0800

    fix: 修改规则弹框

--- assets/Script/Help.ts
+++ assets/Script/Help.ts
@@ -5,62 +5,74 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Help extends cc.Component {
+  @property(cc.Label) toptxt: cc.Label = null;
+  @property(cc.Label) bottomtxt: cc.Label = null;
 
+  @property(cc.Label) title1: cc.Label = null;
+  @property(cc.Label) title2: cc.Label = null;
 
-    @property(cc.Label) toptxt: cc.Label = null;
-    @property(cc.Label) bottomtxt: cc.Label = null;
+  @property(cc.Node) itemGAI: cc.Node = null;
 
-    @property(cc.Label) title1: cc.Label = null;
-    @property(cc.Label) title2: cc.Label = null;
+  @property(cc.Prefab)
+  itemG: cc.Prefab = null;
 
-    @property(cc.Node) itemGAI: cc.Node = null;
+  @property(cc.Node)
+  itemParent: cc.Node = null;
 
+  start() {
+    this.setText();
+    EventMgr.on("onGetGameInfo", this.setText, this);
+    EventMgr.on("queryRatesInfo", this.setItemtXT, this);
+    this.setItemtXT();
+  }
 
-    start() {
-        this.setText();
-        EventMgr.on("onGetGameInfo", this.setText, this);
-         EventMgr.on("queryRatesInfo", this.setItemtXT, this);
-        this.setItemtXT();
+  onDestroy() {
+    EventMgr.off("onGetGameInfo", this.setText, this);
+    EventMgr.off("queryRatesInfo", this.setItemtXT, this);
+  }
+  setText() {
+    if (GameLogic.instance.gameInfo) {
+      let info = GameLogic.instance.gameInfo;
+      let str =
+        info["span1"] +
+        "\n" +
+        "1." +
+        info["span2"] +
+        "\n" +
+        "2." +
+        info["span3"];
+      this.toptxt.string = str;
+      let str2 = info["span6"] + "\n" + info["span7"];
+      this.bottomtxt.string = str2;
+      this.title1.string = info["span4"];
+      this.title2.string = info["span5"];
     }
+  }
 
-    onDestroy() {
-        EventMgr.off("onGetGameInfo", this.setText, this)
-        EventMgr.off("queryRatesInfo", this.setItemtXT, this)
-    }
-    setText() {
-        if (GameLogic.instance.gameInfo) {
-            let info = GameLogic.instance.gameInfo;
-            let str = info["span1"] + "\n" + "1." + info["span2"] + "\n" + "2." + info["span3"];
-            this.toptxt.string = str;
-            let str2 = info["span6"] + "\n" + info["span7"];
-            this.bottomtxt.string = str2;
-            this.title1.string = info["span4"];
-            this.title2.string = info["span5"];
+  setItemtXT() {
+    let list = GameLogic.instance.queryRatesInfo;
+
+    if (Array.isArray(list)) {
+      for (let i = 0; i < list.length; i++) {
+        let temp = cc.instantiate(this.itemG);
+        this.itemParent.addChild(temp);
+        let com = temp.getComponent("HelpItem");
+        if (com) {
+          let isUp = true;
+          let isDown = true;
+          if (i !== 0) {
+            isUp = false;
+          }
+
+          com.setData(list[i], isUp, isDown);
         }
+      }
     }
+  }
 
-    setItemtXT() {
-        if (GameLogic.instance.queryRatesInfo) {
-            for (let i = 0; i < this.itemGAI.children.length; i++) {
-                let item = this.itemGAI.children[i];
-                let rate = GameLogic.instance.queryRatesInfo[i];
-                for (let j = 0; j < item.children.length; j++) {
-                    let items = item.children[j];
-                    let keys = Object.keys(rate);
-                    items.getComponent(cc.Label).string = rate[keys[j]];
-                }
+  onBtnBack() {
+    this.node.destroy();
+  }
 
-            }
-        }
-    }
-
-
-
-
-
-    onBtnBack() {
-        this.node.destroy();
-    }
-
-    // update (dt) {}
+  // update (dt) {}
 }
---
+++ assets/Script/HelpItem.ts
@@ -0,0 +1,52 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class HelpItem extends cc.Component {
+  @property(cc.Label)
+  labName: cc.Label = null;
+
+  @property(cc.Label)
+  labCoin: cc.Label = null;
+
+  @property(cc.Label)
+  labJiLv: cc.Label = null;
+
+  @property(cc.Node)
+  lineUp: cc.Node = null;
+
+  @property(cc.Node)
+  lineDown: cc.Node = null;
+
+  @property({
+    tooltip: "是否显示上线",
+  })
+  public isShowUp: boolean = true;
+
+  @property({
+    tooltip: "是否显示下线",
+  })
+  public isShowDowm: boolean = true;
+
+  onLoad() {
+    this.lineUp.active = this.isShowUp;
+    this.lineDown.active = this.isShowDowm;
+  }
+
+  start() {}
+
+  /**
+   *
+   * @param data
+   * @param showUp
+   * @param isShowDowm
+   */
+  setData(data: any, showUp: boolean, isShowDowm: boolean) {
+    this.labName.string = data["title"];
+    this.labCoin.string = data["reward"].toString();
+    this.labJiLv.string = data["rate"];
+    this.lineUp.active = showUp;
+    this.lineDown.active = isShowDowm;
+  }
+
+  // update (dt) {}
+}
