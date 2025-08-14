ef66f0570c81de68c997eb50ddd360d7b7036bec
hexuejie <hexuejie@jinyikj.com.cn>
2025-08-14 21:22:44 +0800

    fix: 修改礼物大小

--- assets/Script/Shoot.ts
+++ assets/Script/Shoot.ts
@@ -133,8 +133,8 @@ export default class Shoot extends cc.Component {
       GameLogic.instance.loadRemoteSprite(
         gift.giftImage,
         node.getComponent(cc.Sprite),
-        90,
-        70
+        80,
+        80
       );
     }
   }
