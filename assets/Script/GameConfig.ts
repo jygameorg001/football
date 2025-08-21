f367080db59bd45fe10484355e241179c05ede24
xiaoming <541784180@qq.com>
2025-08-21 20:58:54 +0800

    测试

--- assets/Script/GameConfig.ts
+++ assets/Script/GameConfig.ts
@@ -14,9 +14,9 @@ export const GameCfg = {
     gameType: cfg_football2.type, // 114  海外 116 新足球
     url: cfg_football2.ul, //football-api/football
     // 金币足够的  
-    token:"eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxNDQsImxvZ2luX3R5cGUiOjEsInVzZXJfa2V5IjoiYmYzNGU4NTYtYjk1Ny00MTM1LTk3OTUtMDg5NWM2YWJiOGVhIiwidG9rZW5fdHlwZSI6ImFwcCIsInVzZXJuYW1lIjoi55So5oi3Mjk3NDU4In0._90zY6M_zonma_h6HhZPWZMZK6jh9W16O7Jy9LqcKIEyjara-DlB8V4UkY2LxtXEGJf_utTkgot7ppsk2lT3GA",
+    // token:"eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxNDQsImxvZ2luX3R5cGUiOjEsInVzZXJfa2V5IjoiYmYzNGU4NTYtYjk1Ny00MTM1LTk3OTUtMDg5NWM2YWJiOGVhIiwidG9rZW5fdHlwZSI6ImFwcCIsInVzZXJuYW1lIjoi55So5oi3Mjk3NDU4In0._90zY6M_zonma_h6HhZPWZMZK6jh9W16O7Jy9LqcKIEyjara-DlB8V4UkY2LxtXEGJf_utTkgot7ppsk2lT3GA",
     // 金币不足的token 10000223
-    // token:"eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoyMjMsImxvZ2luX3R5cGUiOjEsInVzZXJfa2V5IjoiODY0MGMxZTQtMTgzNC00NWJmLThlNTQtMzE0ZDhlM2Y1Zjk1IiwidG9rZW5fdHlwZSI6ImFwcCIsInVzZXJuYW1lIjoi55So5oi3NDY5MDIwIn0.77b7mMaQMFZ14ZPAkMo3K50slWJWsDodmCn2cG8vAFRqoWephgGKsxpWrQfI3cMxbXNtf6wWuDbyLVzl9zXJZg",
+    token:"eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoyMjMsImxvZ2luX3R5cGUiOjEsInVzZXJfa2V5IjoiODY0MGMxZTQtMTgzNC00NWJmLThlNTQtMzE0ZDhlM2Y1Zjk1IiwidG9rZW5fdHlwZSI6ImFwcCIsInVzZXJuYW1lIjoi55So5oi3NDY5MDIwIn0.77b7mMaQMFZ14ZPAkMo3K50slWJWsDodmCn2cG8vAFRqoWephgGKsxpWrQfI3cMxbXNtf6wWuDbyLVzl9zXJZg",
 }
 
 // [
